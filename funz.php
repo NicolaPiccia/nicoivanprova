e676056204bb12bc84dce199e435335643ace024
ivanpacenti <s1103326@studenti.univpm.it>
2023-04-12 22:22:37 +0200

    commit con php

---
+++ funz.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1 style="text-align: center;">Stampa dati form con elementi a valore multiplo con programma PHP</h1>
+<p>Nome= <?= $_POST["nome"] ?></p>
+<p>Password= <?= $_POST["pass"] ?></p>
+</body>
+</html>
