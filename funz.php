ef9717cf6fff54b1938416e8514992babe7cb374
hex0910 <91594768+ivanpacenti@users.noreply.github.com>
2023-04-13 12:23:12 +0200

    Update funz.php

--- funz.php
+++ funz.php
@@ -4,5 +4,7 @@
 <h1 style="text-align: center;">Stampa dati form con elementi a valore multiplo con programma PHP</h1>
 <p>Nome= <?= $_POST["nome"] ?></p>
 <p>Password= <?= $_POST["pass"] ?></p>
+<?php $somma=$_POST["num1"]+$_POST["num2"]; ?>
+  <p>la somma vale: <?= $somma ?>
 </body>
 </html>
