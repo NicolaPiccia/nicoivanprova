fce6494896cf47d1eb6ff3017d22c35fe11d758a
ivanpacenti <s1103326@studenti.univpm.it>
2023-04-14 22:49:02 +0200

    commit con php

--- funz.php
+++ funz.php
@@ -2,9 +2,26 @@
 <html>
 <body>
 <h1 style="text-align: center;">Stampa dati form con elementi a valore multiplo con programma PHP</h1>
-<p>Nome= <?= $_POST["nome"] ?></p>
-<p>Password= <?= $_POST["pass"] ?></p>
-<?php $somma=$_POST["num1"]+$_POST["num2"]; ?>
-  <p>la somma vale: <?= $somma ?>
+<?php
+session_start();
+if(isset($_POST["invia"])) {
+  if ($_REQUEST["nome"] != null && $_REQUEST["pass"] != null)
+  {
+      if(array_key_exists($_REQUEST["nome"],$_SESSION["lista"])) $_SESSION["lista"][$_REQUEST["nome"]]=$_REQUEST["pass"];
+      else $_SESSION["lista"] += array($_REQUEST["nome"] => $_REQUEST["pass"]);
+  }
+  else echo "Nessuno dei due campi può essere nullo! <br>";
+}
+if(isset($_POST["stampa"]))
+{
+  foreach ($_SESSION["lista"] as $a => $b)
+  {
+    echo "nome utente: ".$a." password: ".$b."<br>";
+  }
+}
+if(isset($_POST["reset"])) {
+    $_SESSION["lista"] = array();
+}
+?>
 </body>
 </html>
